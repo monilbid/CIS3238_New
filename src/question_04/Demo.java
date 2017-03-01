3701b92a472b89ee168144fcfaade9a2af8c4905
Monil Bid <monilbid60@gmail.com>
2017-02-28 23:12:02 -0500

    Question 4 completed.

---
+++ src/question_04/Demo.java
@@ -0,0 +1,35 @@
+
+package question_04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Demo
+{
+    public static void main(String[] args)
+    {
+        CeilingFanPullChain chain = new CeilingFanPullChain();
+        while (true)
+        {
+            System.out.print("Press ");
+            get_line();
+            chain.pull();
+        }
+    }
+    static String get_line()
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in))
+          ;
+        String line = null;
+        try
+        {
+            line = in.readLine();
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+        return line;
+    }
+}
